6fbcd4b5fab1fe62af1a372da1ce40fc4fafa257
Boubker Bribri <boubker.bribri@prestashop.com>
2021-05-25 18:58:05 +0200

    Add mochawesome-report to .gitignore and add configClassMap

---
+++ utils/configClassMap.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    file: 'BO/checkout/index.js',
+    versions: {
+      '1.7.6': '@pages/checkout/index.js',
+      '1.7.7': '@pages/checkout/index.js',
+      '1.7.8': '@pages/checkout/index.js',
+    },
+  },
+  {
+    file: 'BO/checkout/authentication/index.js',
+    versions: {
+      '1.7.6': '@pages/checkout/authentication/index.js',
+      '1.7.7': '@pages/checkout/authentication/index.js',
+      '1.7.8': '@pages/checkout/authentication/index.js',
+    },
+  },
+  {
+    file: 'BO/checkout/authentication/login.js',
+    versions: {
+      '1.7.6': '@pages/checkout/authentication/login.js',
+      '1.7.7': '@pages/checkout/authentication/login.js',
+      '1.7.8': '@pages/checkout/authentication/login.js',
+    },
+  },
+  {
+    file: 'BO/checkout/authentication/additionalInformation.js',
+    versions: {
+      '1.7.6': '@pages/checkout/authentication/additionalInformation.js',
+      '1.7.7': '@pages/checkout/authentication/additionalInformation.js',
+      '1.7.8': '@pages/checkout/authentication/additionalInformation.js',
+    },
+  },
+];
